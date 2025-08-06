c1111150006e55f63e9609b6d7f05957316a095a
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-08-06 12:39:58 +0200

    feat(ex00): add initial megaphone implementation
    • Add `megaphone.cpp` with functionality to convert input to uppercase
    • Introduce `Makefile` for building the `megaphone` program
    • Add `.gitignore` files to exclude build artifacts and binary files
    • Ensure compatibility with C++98 standard and address sanitizers
    
    The `megaphone.cpp` file provides a simple program that outputs uppercase text, handling no-input cases with a default message. The `Makefile` supports flexible builds and includes sanitization options.

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfiorell@student.42nice.fr <lfiorell>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/06 11:42:19 by lfiorell@st       #+#    #+#             */
+/*   Updated: 2025/08/06 11:57:18 by lfiorell@st      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main(int argc, char const *argv[])
+{
+  if (argc < 2)
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  else
+  {
+    for (int i = 1; i < argc; i++)
+    {
+      // create new string and convert to uppercase
+      std::string upper_str;
+      for (const char *c = argv[i]; *c; c++)
+      {
+        upper_str += std::toupper(*c);
+      }
+      std::cout << upper_str;
+    }
+    std::cout << std::endl;
+  }
+  return (0);
+}
