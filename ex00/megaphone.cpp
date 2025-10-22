89cfb626792b34497d400229b0f8feb96c67eb11
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-10-22 13:46:37 +0200

    style: add clang-format and format megaphone.cpp
    Added .clang-format configuration file to enforce consistent C++ code
    formatting across the project.
    
    Applied clang-format to ex00/megaphone.cpp, adjusting brace styles,
    include order, and updated the file timestamp.
    
    No functional changes were made; this is purely stylistic.

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,26 +6,22 @@
 /*   By: lfiorell@student.42nice.fr <lfiorell>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 11:42:19 by lfiorell@st       #+#    #+#             */
-/*   Updated: 2025/08/06 11:57:18 by lfiorell@st      ###   ########.fr       */
+/*   Updated: 2025/10/22 13:22:36 by lfiorell@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cctype>
 #include <iostream>
 #include <string>
-#include <cctype>
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
   if (argc < 2)
     std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-  else
-  {
-    for (int i = 1; i < argc; i++)
-    {
+  else {
+    for (int i = 1; i < argc; i++) {
       // create new string and convert to uppercase
       std::string upper_str;
-      for (const char *c = argv[i]; *c; c++)
-      {
+      for (const char *c = argv[i]; *c; c++) {
         upper_str += std::toupper(*c);
       }
       std::cout << upper_str;
